6104bbcb669d5eddac1c22c2925433b899e17aa8
Lucas-S-S-Cunha <lucassasocunha@gmail.com>
2024-06-11 05:53:20 -0300

    formulação de regras no js

--- scrypt.js
+++ scrypt.js
@@ -6,7 +6,19 @@ function generateNumber() {
     const min = Math.ceil( document.querySelector(".input-min").value)
     const max = Math.floor(document.querySelector(".input-max").value) 
 
-     const result = Math.floor(Math.random() * (max - min + 1)) + min; 
+
+    if (min >= max){
+        alert("o numero minimo não pode ser maior nem igual aou numero maximo")
+    }
+    else {
+             const result = Math.floor(Math.random() * (max - min + 1)) + min; 
 
     alert(result)
+    }
+
+
+
+
+
+
 }
